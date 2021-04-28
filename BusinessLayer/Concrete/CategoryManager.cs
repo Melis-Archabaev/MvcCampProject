eb8474dee2e729ecf7003ccde339d5581bf0e02a
Melis-Archabaev <melis99mrx@gmail.com>
2021-04-29 05:37:43 +0600

    29.04.2021
    Last Update on 29 of April 2021 y.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccesLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repos = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repos.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            //if(p.CategoryName == "" || p.CategoryName.Length<=3 ||p.CategoryDescription == "" ||p.CategoryName.Length >=50)
+            //{
+            //    //Error Message
+            //}
+            //else
+            //{
+                    repos.Insert(p);
+            //}
+        }
+    }
+}
---
+++ MvcCampProject/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCampProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager();
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAllBL();
+            return View(categoryValues);
+        }
+        public ActionResult AddCategory(Category p)
+        {
+            cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
